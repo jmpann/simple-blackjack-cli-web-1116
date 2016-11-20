1bdf75f291f68da7bd9554bd10fdabcd5184e2e5
joshua pann <josh.pann@gmail.com>
2016-11-20 22:40:48 +0000

    Done.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,7 @@
 require_relative "blackjack.rb"
-
-runner
+def run_program
+  welcome
+  current_card_total = initial_round
+  hit?(current_card_total) until current_card_total > 21
+  end_game(current_card_total) 
+end
